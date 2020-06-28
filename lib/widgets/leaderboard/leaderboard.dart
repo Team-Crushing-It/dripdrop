0fbc2dfa65ce0542055b7b63008a5db08b0601cd
mihirKachroo <60433268+mihirKachroo@users.noreply.github.com>
2020-06-28 13:30:07 -0400

    Leaderboard Firebase Data Added

--- lib/widgets/leaderboard/leaderboard.dart
+++ lib/widgets/leaderboard/leaderboard.dart
@@ -2,19 +2,37 @@ import 'package:flutter/material.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
 class Leaderboard extends StatefulWidget {
-  
   @override
   _LeaderboardState createState() => _LeaderboardState();
 }
 
 class _LeaderboardState extends State<Leaderboard> {
   final databaseReference = Firestore.instance;
+  List<String> leaderboardNames = [];
+  List<int> leaderboardScores = [];
 
+  void findTopLeaders() {
+    databaseReference
+        .collection("leaderboard")
+        .orderBy('score')
+        .getDocuments()
+        .then((QuerySnapshot snapshot) {
+      snapshot.documents.forEach((f) {
+        Map<String, dynamic> codedLeaderBoardData = f.data;
+        leaderboardNames.add(codedLeaderBoardData["name"]);
+        leaderboardScores.add(codedLeaderBoardData["score"]);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    print("hitherebuckeroo");
+    findTopLeaders();
+    print(leaderboardNames);
+    print(leaderboardScores);
+
     return Center(
         child: Container(
       margin: const EdgeInsets.only(top: 10.0),
