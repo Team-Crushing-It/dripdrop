13dafcb0514865813cbb872cdcbf04973ebb6176
AgrawalTrisha <agrawaltrisha@gmail.com>
2020-06-28 21:01:58 -0500

    monthly usage

--- lib/widgets/data/monthly_usage.dart
+++ lib/widgets/data/monthly_usage.dart
@@ -8,18 +8,32 @@ class MonthlyUsage extends StatefulWidget {
 }
 
 class _MonthlyUsageState extends State<MonthlyUsage> {
-final databaseReference = Firestore.instance;
+  final databaseReference = Firestore.instance;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Container(
-      margin: const EdgeInsets.only(top: 10.0, bottom:10.0),
-      width: 250,
-      height: 130,
-      color: Colors.red,
-      //Insert goodies here
-      child: null,
-    ));
+      child: Container(
+        margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+        width: 290,
+        height: 130,
+        //Insert goodies here
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("monthly usage", style: TextStyle(fontFamily: "Nasalization", fontWeight: FontWeight.w300, color: Colors.white, fontSize: 20)),
+            RichText(
+              text: TextSpan(
+                style: TextStyle(fontFamily: "Nasalization", color: Theme.of(context).accentColor),
+                children: [
+                  TextSpan(text: "24352", style: TextStyle(fontSize: 55)),
+                  TextSpan(text: "L/m", style: TextStyle(fontSize: 35))
+                ]
+              )
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
