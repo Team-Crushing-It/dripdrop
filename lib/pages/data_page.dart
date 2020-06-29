25949eabd43b92abf29c66f94aa22d1042364123
mihirKachroo <60433268+mihirKachroo@users.noreply.github.com>
2020-06-29 02:51:11 -0400

    Navigation Complete

--- lib/pages/data_page.dart
+++ lib/pages/data_page.dart
@@ -7,54 +7,28 @@ class DataPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          RaisedButton(
-            child: Text('Weekly Graph'),
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  PageTransition(
-                      curve: Curves.linear,
-                      type: PageTransitionType.upToDown,
-                      child: SecondPage()));
-            },
-          ),
-          RaisedButton(
-            child: Text('Monthly Graph'),
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  PageTransition(
-                      curve: Curves.linear,
-                      type: PageTransitionType.upToDown,
-                      child: ThirdPage()));
-            },
-          ),
-        ],
-      ),
-    );
+        backgroundColor: Theme.of(context).backgroundColor,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TimeSelectButton(),
+            Graph(),
+            MonthlyUsage(),
+            RealtimeData(),
+          ],
+        ));
   }
 }
 
 class SecondPage extends StatelessWidget {
-  final String title;
-
-  const SecondPage({Key key, this.title}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final args = ModalRoute.of(context).settings.arguments;
     return Scaffold(
         backgroundColor: Theme.of(context).backgroundColor,
-        appBar: AppBar(
-          title: Text(args ?? "Page Transition Plugin"),
-        ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            TimeSelect(),
+            TimeSelectButton(),
             GraphWeek(),
             MonthlyUsage(),
             RealtimeData(),
@@ -78,11 +52,72 @@ class ThirdPage extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            TimeSelect(),
-            Graph(),
-            MonthlyUsage(),
-            RealtimeData(),
+            RaisedButton(
+              child: Text('Weekly Graph'),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    PageTransition(
+                        curve: Curves.linear,
+                        type: PageTransitionType.upToDown,
+                        child: SecondPage()));
+              },
+            ),
+            RaisedButton(
+              child: Text('Monthly Graph'),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    PageTransition(
+                        curve: Curves.linear,
+                        type: PageTransitionType.upToDown,
+                        child: DataPage()));
+              },
+            ),
           ],
         ));
+  }
+}
+
+class TimeSelectButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print("tapped");
+        Navigator.push(
+            context,
+            PageTransition(
+                curve: Curves.linear,
+                type: PageTransitionType.upToDown,
+                child: ThirdPage()));
+      },
+      child: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+            border: Border.all(
+              color: Theme.of(context).accentColor,
+              width: 5,
+            ),
+            borderRadius: BorderRadius.circular(5)),
+        margin: const EdgeInsets.only(top: 10.0),
+        width: 175,
+        height: 75,
+        //Insert goodies here
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(3),
+              color: Theme.of(context).accentColor),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              "MONTH",
+              style: TextStyle(fontFamily: "Software Tester 7", fontSize: 40),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
