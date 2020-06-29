7b3776994b2e393bcffcba1e8012696175c7064a
AgrawalTrisha <agrawaltrisha@gmail.com>
2020-06-28 22:04:54 -0500

    fonts

--- lib/widgets/dash/alert.dart
+++ lib/widgets/dash/alert.dart
@@ -10,16 +10,22 @@ class Alert extends StatelessWidget {
           margin: const EdgeInsets.only(top: 20.0),
           width: 250,
           height: 70,
+          color: Colors.purple,
           //Insert goodies here
-          child: Image.asset("usage-alert.png"),
+          child: Image(image: AssetImage("usage-alert.png")),
         ),
         Container(
           margin: const EdgeInsets.only(top: 10.0),
           width: 250,
           height: 150,
-          color: Colors.blue,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(3),
+              border: Border.all(
+                color: Theme.of(context).accentColor,
+                width: 5,
+              )),
           //Insert goodies here
-          child: null,
+          child: Text("Your usage increased by 12% this month, exceeding the local average by 2234 L", style: TextStyle(fontFamily: "Nasalization", color: Theme.of(context).accentColor),),
         ),
       ],
     ));
